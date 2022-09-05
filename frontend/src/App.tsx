9e88d7e97758ad038978914b35823608b5b555b8
Laura THAY <laura.thay@ipag.fr>
2022-09-05 15:47:10 +0200

    [add] login button component/ test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
+import Button from './features/login/component/Button.component'
 
 function App() {
 	return (
 		<div>
 			<>App</>
+			<Button />
 		</div>
 	)
 }
