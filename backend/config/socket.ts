42214e569c42490dcba5e389b9d344ac3e384d7d
Verfaillie Mehdi <verfailliemehdi@imac-de-verfaillie.home>
2022-09-07 11:46:29 +0200

    add: base websocket config

---
+++ backend/config/socket.ts
@@ -0,0 +1,12 @@
+/**
+ * Here you can define options for socket.io server described here
+ * https://socket.io/docs/v4/server-options/#socketio-server-options
+ */
+//@ts-ignore - Does not provide type declaration
+import type { WsConfig } from '@ioc:Ruby184/Socket.IO/Ws'
+
+const wsConfig: WsConfig = {
+	//
+}
+
+export default wsConfig
