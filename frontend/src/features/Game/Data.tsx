efb476c54fc565c3bd1598235d09a0838d9a0f16
Vincent <li.jiajinvincent1997@gmail.com>
2022-09-07 17:11:12 +0200

    data file

---
+++ frontend/src/features/Game/Data.tsx
@@ -0,0 +1,23 @@
+const LetterList = [
+	'OP',
+	'RI',
+	'LE',
+	'CH',
+	'LI',
+	'DE',
+	'EA',
+	'TU',
+	'ON',
+	'OV',
+	'IR',
+	'CE',
+	'MO',
+	'WE',
+	'EL',
+	'EX',
+	'PE',
+];
+
+const PlayersList = [{ name: 'test' }, { name: 'test2' }, { name: 'test3' }, { name: 'test4' }];
+
+export { LetterList, PlayersList };
