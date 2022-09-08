0007204b92a1b3524755403ea1a560bb8ceb5315
Vincent <li.jiajinvincent1997@gmail.com>
2022-09-08 14:57:05 +0200

    Create Life.tsx

---
+++ frontend/src/features/Players/components/Life/Life.tsx
@@ -0,0 +1,22 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+
+import LifeLogo from '../../../../style/images/life.svg';
+
+type Props = {
+	life?: number;
+};
+
+function Life(props: Props) {
+	return (
+		<div>
+			{[...new Array(props.life)].map((e, i) => (
+				<span key={i} style={{ marginRight: '2px', marginLeft: '2px' }}>
+					<img src={LifeLogo} alt="logo" />
+				</span>
+			))}
+		</div>
+	);
+}
+
+export default Life;
