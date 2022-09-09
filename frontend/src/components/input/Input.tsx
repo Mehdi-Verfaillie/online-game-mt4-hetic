f3daa7936eb22b09ce5d07dbe3f172a111a48498
Vincent <li.jiajinvincent1997@gmail.com>
2022-09-09 20:10:29 +0200

    fix input

--- frontend/src/components/input/Input.tsx
+++ frontend/src/components/input/Input.tsx
@@ -2,12 +2,27 @@ import React, { useMemo } from 'react'
 import './input.scss'
 import { BaseInputProps } from '@/interfaces/baseInput.interface'
 
-interface Props {
-    placeholder: string;
-    id: string;
-    name: string;
-    type: string;
-    value: string;
-    onKeyPress?: (e:any) => void;
-    onChange?: (e: any) => any;
+export const Input = (props: BaseInputProps<string | null>): JSX.Element => {
+	const formatedValue = useMemo(() => {
+		if (props.value === null || props.value === undefined) return ''
+		return props.value.trim()
+	}, [props.value])
+
+	const forwardChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		if (e.target.value === '') return props.onChange(null)
+		props.onChange(e.target.value)
+	}
+
+	return (
+		<input
+			className="input"
+			type="text"
+			onChange={forwardChange}
+			onKeyPress={props.onKeyPress}
+			placeholder={props.placeholder}
+			id={props.id}
+			name={props.name}
+			value={formatedValue}
+		/>
+	)
 }
