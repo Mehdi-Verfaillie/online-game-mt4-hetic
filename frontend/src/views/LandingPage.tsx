3b460a6d6a71274d3996e058938ed234a0a483fe
Laura THAY <laura.thay@ipag.fr>
2022-09-09 18:43:13 +0200

    [add] connection ID socket

--- frontend/src/views/LandingPage.tsx
+++ frontend/src/views/LandingPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Input } from '@/components/input/Input'
 import { Button } from '@/components/button/Button'
 import './landingpage.scss'
@@ -7,21 +7,80 @@ import { io } from 'socket.io-client'
 export const LandingPage = () => {
 	const [inputValue, setInputValue] = useState<string | null | undefined>('test')
 	const [hasError, setHasError] = useState(false)
+	const [createOrJoin, setCreateOrJoin] = useState<'create' | 'join'>('create')
+	const socket = io('ws://localhost:3000')
+	const idSocket = 'idbidon123'
+
+	// fonction pour checker le nom de l'user s'il existe ou pas
+	const checkNickname = () => {
+		if (inputValue && inputValue.length) {
+			setHasError(false)
+			return false
+		}
+		setHasError(true)
+		return true
+	}
 
 	const joinRoom = () => {
-		//
+		if (!checkNickname()) {
+
+			// rejoingnage de la room
+			socket.emit('join:room', {name: inputValue, room: idSocket})
+			console.log('Rejoingnage de la partie');
+
+			// réception du message d'erreur lors du rejoingnage de la room
+			socket.on('join:room:error', (args) => {
+				console.log(args.message)
+			})
+
+			// réception du message de success lors du rejoingnage de la room
+			socket.on('join:room:success', (...args) => {
+				console.log(...args)
+				// Afficher la view du jeu
+			})
+		}
 	}
 
 	const createRoom = () => {
-		if (inputValue && inputValue.length) {
-			setHasError(false)
-			// CODE SOCKET IO ICI
+		if (!checkNickname()) {
+			// Connect to Socket IO to get the socket ID
+
+			// - creation d'une partie
+			socket.emit('create:room', {name: inputValue})
+			console.log('Creation d"une partieeeeee');
+			
+
+			// réception du message d'erreur sur la creation d'une room
+			socket.on('create:room:error', (...args) => {
+				console.log(...args)
+			})
+
+			// réception du message de success sur la creation d'une room
+			socket.on('create:room:success', (...args) => {
+				console.log(...args)
+				window.history.pushState('data', 'Title', `${idSocket}`)
+				// Afficher la view du jeu
+			})
 			return
 		}
-		setHasError(true)
+	}
+
+	const isCreateOrJoin = () => {
+		if (window.location.pathname === '/') {
+			setCreateOrJoin('create')
+			console.log('création une partie')
+		} else {
+			// Checker si l'id de la room existe
+			setCreateOrJoin('join')
+			console.log('rejoindre une partie')
+		}
 	}
 
 	// const socket = io();
+
+	useEffect(() => {
+		isCreateOrJoin()
+	}, [])
 
 	return (
 		<div className="landing-page">
@@ -29,14 +88,14 @@ export const LandingPage = () => {
 			<Input
 				id="name"
 				name="name"
-				onChange={e => setInputValue(e)}
+				onChange={(e) => setInputValue(e)}
 				placeholder="Entrer votre pseudo"
 				value={inputValue}
 			/>
 			<p className={'landing-page-error-message ' + (hasError && 'show')}>
 				Veuillez choisir un pseudo
 			</p>
-			<Button onClick={() => createRoom()} content="Créer une partie" />
+			<Button onClick={() => {createOrJoin === 'create' ? createRoom() : joinRoom()}} content={createOrJoin === 'create' ? "Créer une partie" : "Rejoindre la partie"} />
 		</div>
 	)
 }
