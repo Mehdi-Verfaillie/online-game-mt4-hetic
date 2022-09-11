0438f830e997c5d23d589bb4a98a36ca52789f0e
Verfaillie Mehdi <verfailliemehdi@imac-de-verfaillie.home>
2022-09-11 17:42:46 +0200

    add: properties

--- backend/src/interfaces/game/game.ts
+++ backend/src/interfaces/game/game.ts
@@ -4,8 +4,9 @@ export interface Game {
   id: string;
   players: Player[];
   status: 'available' | 'ongoing' | 'unavailable';
+  hint: string;
 }
+export type RoundEvent = 'end:countdown' | 'try:answer';
 
 export type NextRoundEvent = 'end:round:fail' | 'end:round:success';
-
 export type NextRoundOutput = { current: number; next: number };
