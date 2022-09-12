cb213878a6c81e2bac0e0fb501e8c8ea2d48aa26
Vincent <li.jiajinvincent1997@gmail.com>
2022-09-12 12:00:18 +0200

    add socket providers

--- frontend/src/providers/socket.provider.tsx
+++ frontend/src/providers/socket.provider.tsx
@@ -1,22 +1,39 @@
-import { join } from '@/features/Game/join/join'
-import React, { Children, createContext } from 'react'
-import { io, Socket } from 'socket.io-client'
-import { create } from '../features/Game/create/create'
+import React, { createContext, useReducer } from 'react';
+import { io } from 'socket.io-client';
+import { listeners as ioListeners } from './listeners';
+import { emitters as ioEmitters } from './emitters';
+import { Player } from '../interfaces/game/player';
 
 interface Props {
-	client: string
-	children: JSX.Element
+  children: JSX.Element;
 }
 
-export const SocketContext = createContext<any>({})
+export const SocketContext = createContext<any>([{}, {}, {}, () => {}]);
+const initialState: { players: Player[]; id: string | null } = {
+  players: [],
+  playerId: null,
+  roomId: null,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'SET_PLAYERS':
+      return { ...state, players: action.value };
+    case 'SET_PLAYER_ID':
+      return { ...state, playerId: action.value };
+    case 'SET_ROOM_ID':
+      return { ...state, roomId: action.value };
+    default:
+      return state;
+  }
+};
 
 export const SocketProvider = (props: Props) => {
-	const socket = io(props.client)
-	return (
-		<SocketContext.Provider
-			value={{ socketConnection: socket, create: create(socket), join: join(socket) }}
-		>
-			{props.children}
-		</SocketContext.Provider>
-	)
-}
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const socket = io('ws://localhost:3333');
+  //   const socket = io(process.env.REACT_APP_SOCKET_URL);
+  const listeners = ioListeners(socket);
+  const emitters = ioEmitters(socket);
+
+  return <SocketContext.Provider value={{ listeners, emitters, state, dispatch }}>{props.children}</SocketContext.Provider>;
+};
